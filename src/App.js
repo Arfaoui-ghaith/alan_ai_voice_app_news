3dd17b3faa2cc2e47dfe65797705a27d73b917b5
Arfaoui-ghaith <ghaith.arfaoui34@gmail.com>
2020-11-02 15:15:31 +0100

    deploy

--- src/App.js
+++ src/App.js
@@ -2,18 +2,48 @@ import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 import NewsCards from './components/NewsCards/NewsCards';
 
+import useStyles from './styles.js';
+import wordsToNumbers from 'words-to-numbers';
 
 function App() {
-
+  const alanLogoSrc = 'https://alan.app/voice/images/previews/preview.jpg';
   const [newsArticles, setNewsArticles] = useState([]);
+  const classes = useStyles();
+  const [activeArticle, setActiveArticle] = useState(-1);
 
   useEffect(() => {
     alanBtn({
       key: '83459e557b5599a1d7b8b753d81411252e956eca572e1d8b807a3e2338fdd0dc/stage',
-      onCommand: ({command, articles}) => {
+      onCommand: ({command, articles, number}) => {
         
         if(command === 'topHeadLines'){
           setNewsArticles(articles);
+          setActiveArticle(-1);
+        } else if(command === 'highlight'){
+          setActiveArticle((prevActiveArticle) => prevActiveArticle + 1);
+        } else if(command === 'open') {
+    
+          if(isNaN(parseInt(number))){
+            if(wordsToNumbers(number, {fuzzy: true}) > articles.length){
+              alanBtn().playText(`article number ${wordsToNumbers(number, {fuzzy: true})} does'nt exist, we only have ${articles.length} articles`);
+            } else { 
+              if(wordsToNumbers(number, {fuzzy: true}) === undefined){
+                alanBtn().playText(`you didn't said the number correctly, try again!`);
+              }
+              else{window.open(articles[wordsToNumbers(number, {fuzzy: true})].url, '_blank');}
+              
+              }
+          }
+          else {
+            if(number > articles.length){
+              alanBtn().playText(`article number ${number} does'nt exist, we only have ${articles.length} articles`);
+            } else { 
+              window.open(articles[number].url, '_blank');
+                   }
+          }
+
+          
+          
         }
       },
     });
@@ -21,8 +51,10 @@ function App() {
 
   return (
     <div className="App">
-     <h1>Alan AI News Application</h1>
-     <NewsCards articles={newsArticles} />
+     <div className={classes.logoContainer}>
+      <img  src={alanLogoSrc} className={classes.alanLogo} alt="alan logo"/>
+     </div>
+     <NewsCards key={activeArticle} articles={newsArticles} activeArticle={activeArticle} />
     </div>
   );
 }
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,29 +1,44 @@
-import React from 'react';
+import React, {useEffect, useState, createRef} from 'react';
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import useStyles from './styles.js'
+import classNames from 'classnames';
 
 export default function NewsCard(props) {
     const classes = useStyles();
+    const [elRefs, setElRefs] = useState([]);
+    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+    useEffect(() => {
+      setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+    }, []);
+
+    useEffect(() => {
+      if(props.i === props.activeArticle && elRefs[props.activeArticle]){
+        scrollToRef(elRefs[props.activeArticle]);
+      }
+    }, [props.i, props.activeArticle, elRefs])
+
     return (
-        <Card className={classes.root}>
+        <Card ref={elRefs[props.i]} className={classNames(classes.card, props.activeArticle === props.i ? classes.activeCard : null)}>
         <CardActionArea href={props.article.url} target="_blank">
           <CardMedia
+            className={classes.media}
             component="img"
-            alt="Contemplative Reptile"
-            height="140"
-            image={props.article.urlToImage}
-            title="Contemplative Reptile"
+            
+            image={props.article.urlToImage || 'https://www.stlucianewsonline.com/wp-content/uploads/2013/11/Latest-News-415x250.jpg'} 
           />
+          <div className={classes.details}>
+            <Typography variant="body2" color="textSecondary" component="h2"></Typography>
+            <Typography variant="body2" color="textSecondary" component="h2">{props.article.source.name}</Typography>
+          </div>
+          <Typography variant="h5" className={classes.title} gutterBottom>{props.article.title}</Typography>
           <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              {props.article.title}
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-            {props.article.description}
+            <Typography color="textSecondary" variant="body2" component="p">
+              {props.article.description}
             </Typography>
           </CardContent>
         </CardActionArea>
-        <CardActions>
+        <CardActions className={classes.cardActions}>
           <Button size="small" color="primary">
             Learn More
           </Button>
